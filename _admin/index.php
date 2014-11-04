095beb91b65e41a4ef82099a45ed89014350d785
Pieter De Norre <pieter.de.norre@telenet.be>
2014-11-04 16:56:20 +0100

    v2.7
    Admin Page Start

---
+++ _admin/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>     <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>       <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>       <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Lannoo ControlPanel</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="img/ico/favicon.ico" rel="shortcut icon"/>
+	<link href="../_css/normalize.css" rel="stylesheet" type="text/css" />
+	<link href="css/admin.css" rel="stylesheet" type="text/css" />
+	
+	<link href="_img/ico/favicon.ico" rel="shortcut icon"/>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js" type="text/javascript"></script>
+	<script>var __adobewebfontsappname__ = "code"</script>
+	<script src="http://use.edgefonts.net/comfortaa:n4,n3,n7:all;open-sans:n7,i7,n8,i8,i4,n3,i3,n4,n6,i6:all.js"></script>
+
+</head>
+mhsuwbdmvobhjqsmoduvbh
+			
+</body>
+</html>
