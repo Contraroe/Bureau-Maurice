9a25b522f9c36f99ec0a4ba5757bf8f7c4711b45
Pieter De Norre <pieter.de.norre@telenet.be>
2014-11-18 15:59:47 +0100

    v3.3
    admin

--- _admin/_includes/all_data.php
+++ _admin/_includes/all_data.php
@@ -1,9 +1,11 @@
+<?php $ref = $_REQUEST['ref']; ?>
 
-	<tr>
+
+	<tr class="<?php if ($ref == $f5) { echo 'active';} ?>">
 		<td><?php echo htmlspecialchars_decode($f5); ?></td> 
 		<td><?php echo htmlspecialchars_decode($f4); ?></td>
 		<td><?php echo htmlspecialchars_decode($f1); ?></td>
-		<td><?php echo htmlspecialchars_decode($f2); ?> &euro; </td>
+		<td><?php echo htmlspecialchars_decode($f2); ?> &euro;</td>
 		<td><?php echo htmlspecialchars_decode($f6); ?></td>
 		<td><a href="index.php?ref=<?php echo htmlspecialchars_decode($f5); ?>">+</a></td>
 	</tr>
---
+++ db_config.php
@@ -0,0 +1,14 @@
+<?php
+
+//online
+$dbhost="localhost";
+$dbuser="root";
+$dbpass="root";
+$database="maurice";
+
+//local
+// $dbhost="localhost";
+// $dbuser="drukkqf12_lannoo";
+// $dbpass="maurice";
+// $database="drukkqf12_lannooworld2";
+?>
