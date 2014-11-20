9f6c6373edbe37ee61d71a6872efd2403a6dfd6d
Pieter De Norre <pieter.de.norre@telenet.be>
2014-11-20 17:00:42 +0100

    v3.3
    Front an back online

--- _includes/frit_small.php
+++ _includes/frit_small.php
@@ -6,7 +6,7 @@
 			<img src="_img/zaken/<?php echo htmlspecialchars_decode($f10);?>" alt="<?php echo htmlspecialchars_decode($f10); ?>">
 		</div>
 		<h6><?php echo htmlspecialchars_decode($f7); ?> </h6>
-		<div id="text"><?php echo htmlspecialchars_decode($f1); ?></div><br><br>
+		<div id="text"><?php echo htmlspecialchars_decode($f1); ?></div>
 		<b>Prijs</b> : <?php echo htmlspecialchars_decode($f3); ?> &euro;<br>
 		<b>Huur</b> : <?php echo htmlspecialchars_decode($f4); ?> &euro;
 		
