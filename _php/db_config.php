c45e08fe50d2d6363a7fe559c9cd64f9243ffcff
Pieter De Norre <pieter.de.norre@telenet.be>
2014-11-26 14:19:18 +0100

    V3.6

--- _php/db_config.php
+++ _php/db_config.php
@@ -7,7 +7,7 @@
 // $database="maurice";
 
 //local
-$dbhost="159.253.0.15:3306";
+$dbhost="159.253.0.15";
 $dbuser="drukkqf12_lannoo";
 $dbpass="maurice";
 $database="drukkqf12_lannooworld2";
--- _php/db_connect.php
+++ _php/db_connect.php
@@ -1,6 +1,9 @@
 <?php
-$connect=mysql_connect($dbhost,$dbuser,$dbpass) or die("Error connecting to Database!");
-mysql_select_db($database,$connect) or die("Cannot select database!");
-mysql_query("SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'");  
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
+	$connect=mysql_connect($dbhost,$dbuser,$dbpass) or die("Error connecting to Database!");
+	mysql_select_db($database,$connect) or die("Cannot select database!");
+	mysql_query("SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'");  
 
 ?>
