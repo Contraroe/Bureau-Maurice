3f28dc05364be4a2484845d0f69d2fad502d24a3
Pieter De Norre <pieter.de.norre@telenet.be>
2014-11-27 15:40:53 +0100

    v3.7

--- _php/db_config.php
+++ _php/db_config.php
@@ -2,9 +2,9 @@
 
 //online
 // $dbhost="localhost";
-// $dbuser="root";
-// $dbpass="root";
-// $database="maurice";
+// $dbuser="drukkqf12_lannoo";
+// $dbpass="maurice";
+// $database="drukkqf12_lannooworld2";
 
 //local
 $dbhost="159.253.0.15";
